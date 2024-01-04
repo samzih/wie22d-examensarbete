ab240f83066ec73350ac4b255ce81a0d2b09fd0a
samzih <samzih1337@gmail.com>
2024-01-04 14:13:53 +0100

    Product cards implemented to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useProductContext } from '../context/ProductContext';
+import { Container, Row, Col } from 'react-bootstrap';
+import FeaturedProductCard from '../components/ui/FeaturedProductCard';
+import StandardProductCard from '../components/ui/StandardProductCard';
+import shuffle from '../utils/shuffleArray';
+
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    const { products, fetchProducts } = useProductContext();
+
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const featuredProducts = shuffle(products).slice(0, 3);
+
+
+    return (
+        <>
+            <Container fluid='md' className='mt-5'>
+                <Row xxl={3}>
+                    {featuredProducts.map((product, id) => (
+                        <Col key={id} lg={true}>
+                            <FeaturedProductCard title={product.name} text={product.description} img={product.images[0]} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+
+            <Container fluid='md' className='mt-5'>
+                <Row xxl={3}>
+                    {products.map((product, id) => (
+                        <Col key={id} lg={true}>
+                            <StandardProductCard img={product.images[0]} title={product.name} subtitle={product.description} cpu={product.metadata.processor} gpu={product.metadata.graphics} ram={product.metadata.ram} price={product.default_price.unit_amount} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </>
+    )
 }
 
 export default Home
