b835b4a35c0cdeaa8852ed055aafa068f4fbdfbe
samzih <samzih1337@gmail.com>
2024-01-07 20:27:43 +0100

    Client side pagination for standard products

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,38 @@
 import { useEffect, useState } from 'react';
 import { useProductContext } from '../context/ProductContext';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Pagination } from 'react-bootstrap';
 import FeaturedProductCard from '../components/ui/FeaturedProductCard';
 import StandardProductCard from '../components/ui/StandardProductCard';
-import shuffle from '../utils/shuffleArray';
+import shuffleArray from '../utils/shuffleArray';
 
 
 function Home() {
     const { products, fetchProducts } = useProductContext();
-
+    const [currentPage, setCurrentPage] = useState(1);
+    const [featuredProducts, setFeaturedProducts] = useState([]);
 
     useEffect(() => {
         fetchProducts();
     }, []);
 
-    const featuredProducts = shuffle(products).slice(0, 3);
+    useEffect(() => {
+        setFeaturedProducts(shuffleArray(products).slice(0, 3));
+    }, [products]);
+
+    const handlePageChange = (page) => {
+
+        if (typeof (page) !== 'number') {
+            return
+        }
+
+        setCurrentPage(page);
+    }
+
+    // Calculate the indexes of the current page
+    const productsPerPage = 3;
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
 
 
     return (
@@ -31,12 +49,23 @@ function Home() {
 
             <Container fluid='md' className='mt-5'>
                 <Row xxl={3}>
-                    {products.map((product, id) => (
+                    {currentProducts.map((product, id) => (
                         <Col key={id} lg={true}>
                             <StandardProductCard img={product.images[0]} title={product.name} subtitle={product.description} cpu={product.metadata.processor} gpu={product.metadata.graphics} ram={product.metadata.ram} price={product.default_price.unit_amount} />
                         </Col>
                     ))}
                 </Row>
+
+                <Pagination className='justify-content-center my-3'>
+                    <Pagination.Prev onClick={() => handlePageChange(currentPage > 1 && currentPage - 1)} />
+                    {Array.from({ length: Math.ceil(products.length / productsPerPage) }).map((_, index) => (
+                        <Pagination.Item key={index + 1} active={index + 1 === currentPage} onClick={() => handlePageChange(index + 1)}>
+                            {index + 1}
+                        </Pagination.Item>
+                    ))}
+                    <Pagination.Next onClick={() => handlePageChange(currentPage < products.length / productsPerPage && currentPage + 1)} />
+                </Pagination>
+
             </Container>
         </>
     )
