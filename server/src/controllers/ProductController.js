78db34fdfa51158dade5694f76f070b87195e494
samzih <samzih1337@gmail.com>
2024-01-10 22:47:13 +0100

    Retrieves a product from Stripe API

--- server/src/controllers/ProductController.js
+++ server/src/controllers/ProductController.js
@@ -20,4 +20,21 @@ const getActiveProducts = async (req, res) => {
     }
 }
 
-module.exports = { getActiveProducts };
+const getProduct = async (req, res) => {
+    try {
+
+        const product = await stripe.products.retrieve(req.params.id, {
+            expand: ['default_price'],
+        });
+
+        res.status(200).json(product);
+
+    } catch (error) {
+
+        console.log(error.message);
+        res.status(400).send(`There seems to be a problem with retrieving the product from stripe. <br> ERROR MESSAGE: ${error.message}`);
+
+    }
+}
+
+module.exports = { getActiveProducts, getProduct };
