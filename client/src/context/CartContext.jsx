167d9064b1d1e084e7fa790ac7d1b0d8b4e463c4
samzih <samzih1337@gmail.com>
2024-01-14 22:45:05 +0100

    Cart items CRUD operations

--- client/src/context/CartContext.jsx
+++ client/src/context/CartContext.jsx
@@ -5,12 +5,23 @@ const CartContext = createContext({
     cartItems: [],
     setCartItems: () => { },
     addToCart: () => { },
+    removeFromCart: () => { },
+    increaseQuantity: () => { },
+    decreaseQuantity: () => { },
+    emptyCart: () => { },
+    cartTotalPrice: 0,
 });
 
 export const useCartContext = () => useContext(CartContext);
 
 const CartProvider = ({ children }) => {
     const [cartItems, setCartItems] = useLocalStorage('cart', []);
+
+
+    // Calculate total price of all cart items (incl. quantity)
+    const cartTotalPrice = cartItems.reduce((acc, currentItem) => (
+        acc + (currentItem.product.default_price.unit_amount / 100) * currentItem.quantity
+    ), 0);
 
 
     const addToCart = (product) => {
@@ -23,15 +34,52 @@ const CartProvider = ({ children }) => {
             existingItem.quantity++;
             setCartItems([...cartItems]);
         } else {
-            setCartItems([...cartItems, { product: product, quantity: 1 }])
+            setCartItems([...cartItems, { product, quantity: 1 }])
         }
 
     }
 
 
+    const removeFromCart = (productID) => {
+        const updatedCart = cartItems.filter(item => item.product.id !== productID);
+        setCartItems(updatedCart);
+    }
+
+
+    const increaseQuantity = (product) => {
+
+        const currentItem = cartItems.find(item => item.product.id === product.id);
+
+        if (currentItem) {
+            currentItem.quantity++;
+            setCartItems([...cartItems]);
+        }
+
+    }
+
+
+    const decreaseQuantity = (product) => {
+
+        const currentItem = cartItems.find(item => item.product.id === product.id);
+
+        if (currentItem.quantity > 1) {
+            currentItem.quantity--;
+            setCartItems([...cartItems]);
+        } else {
+            removeFromCart(product.id);
+        }
+
+    }
+
+
+    const emptyCart = () => {
+        setCartItems([]);
+    }
+
+
     return (
         <div>
-            <CartContext.Provider value={{ cartItems, setCartItems, addToCart }}>
+            <CartContext.Provider value={{ cartItems, setCartItems, addToCart, removeFromCart, increaseQuantity, decreaseQuantity, emptyCart, cartTotalPrice }}>
                 {children}
             </CartContext.Provider>
         </div>
